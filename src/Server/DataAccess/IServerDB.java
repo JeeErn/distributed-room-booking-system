787547e28232d6ae45a2c17607c7f680fdbc20f2
JeeErn <jee.ern88@gmail.com>
2021-03-02 21:23:45 +0800

    Update ServerDB - Add JavaDocs descriptors to IServerDB - Implement createBooking and updateBooking methods in ServerDB - Create initial facilities within ServerDB on construction

--- src/Server/DataAccess/IServerDB.java
+++ src/Server/DataAccess/IServerDB.java
@@ -1,6 +1,28 @@
 package Server.DataAccess;
 
+import Server.Exceptions.BookingNotFoundException;
+import Server.Exceptions.FacilityNotFoundException;
+
 public interface IServerDB {
-    public void createBooking();
-    public void updateBooking();
+    /**
+     * Creates a booking for a given day for a given facility between a specified start to end time
+     * @param day: the int value of the enumerated days
+     * @param clientId: the client id string
+     * @param facilityName: the name of the facility to book
+     * @param startTime: the start time in HH:mm
+     * @param endTime: the end time in HH:mm
+     * @return the confirmation id of the booking created
+     * @throws FacilityNotFoundException when the facility name provided is not found
+     */
+    String createBooking(int day, String clientId, String facilityName, String startTime, String endTime) throws FacilityNotFoundException;
+
+    /**
+     * Updates a booking given an existing confirmation id and the new start and end times
+     * @param confirmationId: confirmation id of an existing, confirmed booking
+     * @param facilityName: the name of the facility
+     * @param newStartTime: the new start time in HH:mm
+     * @param newEndTime: the new end time in HH:mm
+     * @throws BookingNotFoundException when the confirmation id is not found in the facility
+     */
+    void updateBooking(String confirmationId, String facilityName, String newStartTime, String newEndTime) throws BookingNotFoundException;
 }
--- src/Server/DataAccess/ServerDB.java
+++ src/Server/DataAccess/ServerDB.java
@@ -1,7 +1,69 @@
 package Server.DataAccess;
 
-// TODO: implement methods
-public class ServerDB implements IServerDB{
-    public void createBooking(){}
-    public void updateBooking(){}
+import Server.Entities.Concrete.Facility;
+import Server.Exceptions.BookingNotFoundException;
+import Server.Exceptions.FacilityNotFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ServerDB implements IServerDB {
+    private HashMap<String, Facility> facilities;
+    private HashMap<String, Integer> bookingsByDay;
+
+    public ServerDB() {
+        facilities = createFacilitiesTable();
+        bookingsByDay = new HashMap<>();
+    }
+
+    public List<String> getFacilityNames() {
+        return new ArrayList<>(facilities.keySet());
+    }
+
+    @Override
+    public String createBooking(int day, String clientId, String facilityName, String startTime, String endTime)
+            throws FacilityNotFoundException {
+        if (!facilities.containsKey(facilityName)) {
+            throw new FacilityNotFoundException("Facility does not exist");
+        }
+        Facility facility = facilities.get(facilityName);
+        String confirmationId = facility.addBooking(day, clientId, startTime, endTime);
+        bookingsByDay.put(confirmationId, day);
+        return confirmationId;
+    }
+
+    @Override
+    public void updateBooking(String confirmationId, String facilityName, String newStartTime, String newEndTime)
+            throws BookingNotFoundException {
+        Facility facility = facilities.get(facilityName);
+        int day = bookingsByDay.get(confirmationId);
+        facility.updateBooking(day, confirmationId, newStartTime, newEndTime);
+    }
+
+    private HashMap<String, Facility> createFacilitiesTable() {
+        HashMap<String, Facility> facilities = new HashMap<>();
+        List<String[]> allFacilityInfo = getFacilityInfo();
+        for (String[] facilityInfo : allFacilityInfo) {
+            String facilityName = facilityInfo[0];
+            String facilityType = facilityInfo[1];
+            Facility facility = new Facility(facilityName, facilityType);
+            facilities.put(facilityName, facility);
+        }
+        return facilities;
+    }
+
+    private List<String[]> getFacilityInfo() {
+        List<String[]> facilityInfo = new ArrayList<>();
+        // Add facilities in the form [facilityName, facilityType]
+        facilityInfo.add(new String[]{"LT1", "Lecture Theater"});
+        facilityInfo.add(new String[]{"LT2", "Lecture Theater"});
+        facilityInfo.add(new String[]{"TC1", "Tennis Court"});
+        facilityInfo.add(new String[]{"BTC1", "Badminton Court"});
+        facilityInfo.add(new String[]{"BTC2", "Badminton Court"});
+        facilityInfo.add(new String[]{"SWLAB1", "Software Lab"});
+
+        return facilityInfo;
+    }
+
 }
