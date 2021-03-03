a6da0e1d0351cecbb59f353f415c12996fbfae6e
JeeErn <jee.ern88@gmail.com>
2021-03-03 15:08:11 +0800

    Update FacilityTest - Add testcase for update booking causing sorted order retrieved to change

--- src/Test/Entities/FacilityTest.java
+++ src/Test/Entities/FacilityTest.java
@@ -6,10 +6,13 @@ import Server.Exceptions.BookingNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FacilityTest {
@@ -79,5 +82,43 @@ public class FacilityTest {
         String falseConfirmationId = "Fake";
         Exception e = assertThrows(BookingNotFoundException.class, () -> facility.updateBooking(2, falseConfirmationId, "00:00", "00:01"));
         assertEquals("No booking under confirmationId: Fake", e.getMessage());
+    }
+
+    @Test
+    public void testUpdateBookingChangesSortedBookingsOrder() {
+        int day = 3;
+        String clientIdToChange = "Dummy client 1";
+        String clientIdUnchange = "Dummy client 2";
+        String newStartTime = "10:00";
+        String startTimeUnchanged = "12:00";
+        String startTimeToChange = "15:00";
+        String newEndTime = "11:59";
+        String endTimeUnchanged = "14:00";
+        String endTimeToChange = "17:00";
+        try {
+            String confirmationIdToChange = facility.addBooking(day, clientIdToChange, startTimeToChange, endTimeToChange);
+            facility.addBooking(day, clientIdUnchange, startTimeUnchanged, endTimeUnchanged);
+            // Test initial order
+            testSortedStartTimings(day, startTimeUnchanged, startTimeToChange);
+            // Update booking
+            boolean success = facility.updateBooking(day, confirmationIdToChange, newStartTime, newEndTime);
+            assertTrue(success);
+            // Test final order
+            testSortedStartTimings(day, newStartTime, startTimeUnchanged);
+        } catch (Exception e) {
+            System.out.println("You should not reach here");
+        }
+    }
+
+    private void testSortedStartTimings(int day, String earlier, String later) {
+        List<IBooking> sortedBookings = facility.getBookingsSorted(day);
+        List<String> timingOrder = (
+                sortedBookings
+                .stream()
+                .map(booking -> booking.getStartTime())
+                .collect(Collectors.toList())
+        );
+        List<String> expectedTimingOrder = Arrays.asList(earlier, later);
+        assertIterableEquals(expectedTimingOrder, timingOrder);
     }
 }
