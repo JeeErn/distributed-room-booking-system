eb8d5a6127c0922b5872f00fbb837f8addc65104
gwynethang <gwynethaxy@gmail.com>
2021-03-04 17:34:27 +0800

    marshall unmarshall String, boolean, short, float, double

--- src/Marshaller/Marshaller.java
+++ src/Marshaller/Marshaller.java
@@ -5,7 +5,12 @@ import java.nio.ByteBuffer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-//import com.google.common.primitives.Bytes;
+/***
+ * Done: Integer, String, Boolean, Short, Float, Double
+ * In Progress:
+ * Haven't Start: Class, List, Object
+***/
+
 
 public class Marshaller {
 
@@ -61,25 +66,110 @@ public class Marshaller {
     }
 
     private static String unmarshallString(List<Byte> seqBytes){
+        int start = intToByteList(byteListToInt(seqBytes)).size();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i< byteListToInt(seqBytes); i++){
+        for (int i = start; i< (seqBytes.size()); i++){
             sb.append((char) seqBytes.get(i).byteValue());
-            System.out.println((char) seqBytes.get(i).byteValue());
         }
         return sb.toString();
     }
 
+    private static void marshallBoolean(boolean bool, List<Byte> seqBytes){
+        byte b = (byte) (bool? 1:0);
+        seqBytes.add(b);
+    }
+
+    private static boolean unmarshallBoolean(List<Byte> seqBytes){
+        return seqBytes.get(0) != (byte) 0;
+    }
+
+    private static void marshallShort(short shrt, List<Byte> seqBytes){
+        ByteBuffer bb = ByteBuffer.allocate(Short.BYTES);
+        byte[] arr = bb.putShort(shrt).array();
+        seqBytes.addAll(IntStream.range(0, arr.length).mapToObj(i -> arr[i]).collect(Collectors.toList()));
+    }
+
+    private static short unmarshallShort(List<Byte> seqBytes){
+        byte[] arr = new byte[Short.BYTES];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = seqBytes.get(i);
+        }
+        ByteBuffer bb = ByteBuffer.wrap(arr);
+        short shrt = bb.getShort();
+        return shrt;
+    }
+
+    private static void marshallFloat(float flt, List<Byte> seqBytes){
+        ByteBuffer bb = ByteBuffer.allocate(Float.BYTES);
+        byte[] arr = bb.putFloat(flt).array();
+        seqBytes.addAll(IntStream.range(0, arr.length).mapToObj(i -> arr[i]).collect(Collectors.toList()));
+    }
+
+    private static float unmarshallFloat(List<Byte> seqBytes){
+        byte[] arr = new byte[Float.BYTES];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = seqBytes.get(i);
+        }
+        ByteBuffer bb = ByteBuffer.wrap(arr);
+        float flt = bb.getFloat();
+        return flt;
+    }
+
+    private static void marshallDouble(double dbl, List<Byte> seqBytes){
+        ByteBuffer bb = ByteBuffer.allocate(Double.BYTES);
+        byte[] arr = bb.putDouble(dbl).array();
+        seqBytes.addAll(IntStream.range(0, arr.length).mapToObj(i -> arr[i]).collect(Collectors.toList()));
+    }
+
+    private static double unmarshallDouble(List<Byte> seqBytes){
+        byte[] arr = new byte[Double.BYTES];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = seqBytes.get(i);
+        }
+        ByteBuffer bb = ByteBuffer.wrap(arr);
+        double dbl = bb.getDouble();
+        return dbl;
+    }
+
     public static void main(String[] args){
-//        int testInt = 1001232323;
-//        System.out.println(testInt == byteListToInt(intToByteList(testInt)));
+        int testInt = 1001232323;
+        System.out.println(testInt == byteListToInt(intToByteList(testInt)));
 
-        String testString = "Hello";
-        System.out.println(testString.getBytes());
-
+        String testString = "Distributed Systems Project";
         List<Byte> seqBytes = new ArrayList<>();
         marshallString(testString, seqBytes);
-        System.out.println(unmarshallString(seqBytes));
-        System.out.println(byteListToInt(seqBytes));
+        String actualString = unmarshallString(seqBytes);
+        System.out.println(testString.equals(actualString) );
+
+        boolean testTrue = true;
+        List<Byte> seqBytesTestTrue = new ArrayList<>();
+        marshallBoolean(testTrue, seqBytesTestTrue);
+        boolean actualTrue = unmarshallBoolean(seqBytesTestTrue);
+        System.out.println("Test Boolean True: " + (testTrue == actualTrue));
+
+        boolean testFalse = false;
+        List<Byte> seqBytesTestFalse = new ArrayList<>();
+        marshallBoolean(testFalse, seqBytesTestFalse);
+        boolean actualFalse = unmarshallBoolean(seqBytesTestFalse);
+        System.out.println("Test Boolean False: " + (testFalse == actualFalse));
+
+        short testShort = 9385;
+        List<Byte> seqBytesTestShort = new ArrayList<>();
+        marshallShort(testShort,seqBytesTestShort);
+        short actualShort = unmarshallShort(seqBytesTestShort);
+        System.out.println("Test Short: " + (testShort == actualShort));
+
+        float testFloat = 69.420f;
+        List<Byte> seqBytesTestFloat = new ArrayList<>();
+        marshallFloat(testFloat, seqBytesTestFloat);
+        float actualFloat = unmarshallFloat(seqBytesTestFloat);
+        System.out.println("Test Float: " + (testFloat == actualFloat));
+
+        double testDouble = 93478547;
+        List<Byte> seqBytesTestDouble = new ArrayList<>();
+        marshallDouble(testDouble, seqBytesTestDouble);
+        double actualDouble = unmarshallDouble(seqBytesTestDouble);
+        System.out.println("Test Double: " + (testDouble == actualDouble));
 
     }
 
