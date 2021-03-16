9990343cd209687de06167dfb5799f3da88ec3de
JeeErn <jee.ern88@gmail.com>
2021-03-16 15:53:40 +0800

    Create API skeleton - Add switch-case to route request - Create skeletons for all handler functions - Remove facility object as testing phase is over - Collapse duplicate catch blocks

--- src/Server/Application/Server.java
+++ src/Server/Application/Server.java
@@ -1,20 +1,20 @@
 package Server.Application;
 
 import Server.BusinessLogic.FacilitiesBookingSystem;
-import Server.BusinessLogic.IBookingSystem;
 import Server.DataAccess.IServerDB;
 import Server.DataAccess.ServerDB;
-import Server.Entities.Concrete.CallbackTestFacility;
-import Server.Entities.IObservable;
+import Server.Exceptions.*;
 
 import java.io.IOException;
 import java.net.*;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Server {
     private DatagramSocket socket;
     private IServerDB serverDB;
     private FacilitiesBookingSystem facilitiesBookingSystem;
-    private IObservable facility; // TODO: Remove after testing phase
 
     public Server(int port) {
         try {
@@ -22,11 +22,8 @@ public class Server {
             socket = new DatagramSocket(port);
             serverDB = new ServerDB();
             facilitiesBookingSystem = new FacilitiesBookingSystem(serverDB);
-            facility = new CallbackTestFacility("Test Facility");
             printIp();
-        } catch (SocketException e){
-            System.out.println(e);
-        } catch (Exception e) {
+        } catch (Exception e){
             System.out.println(e);
         }
     }
@@ -55,30 +52,121 @@ public class Server {
             DatagramPacket request = new DatagramPacket(buffer, buffer.length);
             socket.receive(request);
 
-            // Pseudo client request
-            System.out.println(request);
-
             // TODO: Unmarshall the client request
-            // TODO: Process the client request and call the business logic layer
+            int functionCode = 0; // "unmarshallFunction(request.getData())"
+            // TODO: explore using enums
+            String responseMessage;
+            switch (functionCode) {
+                case 0:
+                    responseMessage = handleGetAvailability(request);
+                    break;
+                case 1:
+                    responseMessage = handleCreateBooking(request);
+                    break;
+                case 2:
+                    responseMessage = handleUpdateBooking(request);
+                    break;
+                case 3:
+                    responseMessage = handleAddObservingClient(request);
+                    break;
+                default:
+                    responseMessage = handleHeartbeat();
+                    break;
+            }
 
             InetAddress clientAddress = request.getAddress();
             int clientPort = request.getPort();
 
-            // Test callback
-            long expiration = System.currentTimeMillis() + 30000;
-            facility.addObservationSession(clientAddress, clientPort, expiration);
-            facility.sendUpdateToObservingClients(socket);
-
             // Pseudo server response
-            String data = "Message from server";
-            buffer = data.getBytes();
-            
+            buffer = responseMessage.getBytes();
+
             DatagramPacket response = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
             socket.send(response);
-
-
-
         }
+    }
+
+    // ===================================
+    // Handler functions
+    // ===================================
+    private String handleGetAvailability(DatagramPacket request) {
+        try {
+            String facilityName = "unmarshallString(request.getData())";
+            List<Integer> days = Arrays.asList(0, 1); // unmarshallList(request.getData());
+            String availability = facilitiesBookingSystem.getAvailability(facilityName, days);
+            return "Facility availability: " + availability;
+        } catch (FacilityNotFoundException e) {
+            return "404: Facility not found";
+        } catch (ParseException e) {
+            return "500: Error occurred retrieving availability";
+        }
+    }
+
+    private String handleCreateBooking(DatagramPacket request) {
+        try {
+            String requestId = "unmarshallString(request.getData())";
+            if (requestId == "exists in cache") {
+                return "retrieve response from cache";
+            }
+            String facilityName = "unmarshallString(request.getData())";
+            String startDateTime = "unmarshallString(request.getData())";
+            String endDateTime = "unmarshallString(request.getData())";
+            String clientId = generateClientIdFromOrigin(request);
+            String confirmationId = facilitiesBookingSystem.createBooking(facilityName, startDateTime, endDateTime, clientId, socket);
+            return "Booking confirmation ID: " + confirmationId;
+        } catch (InvalidDatetimeException | ParseException e) {
+            return "400: Invalid datetime provided";
+        } catch (TimingUnavailableException e) {
+            return "409: Booking time not available";
+        } catch (FacilityNotFoundException e) {
+            return"404: Facility not found";
+        }
+    }
+
+    private String handleUpdateBooking(DatagramPacket request) {
+        try {
+            String requestId = "unmarshallString(request.getData())";
+            if (requestId == "exists in cache") {
+                return "retrieve response from cache";
+            }
+            String confirmationId = "unmarshallString(request.getData())";
+            String clientId = generateClientIdFromOrigin(request);
+            int offset = 0; // unmarshallInt(request.getData())
+            facilitiesBookingSystem.updateBooking(confirmationId, clientId, offset, socket);
+            return "Booking updated successfully";
+        } catch (WrongClientIdException | BookingNotFoundException e) {
+            return "404: Invalid confirmation ID";
+        } catch (InvalidDatetimeException e) {
+            return "400: Invalid offset provided";
+        } catch (TimingUnavailableException e) {
+            return "409: New booking time not available";
+        } catch (ParseException e) {
+            return "500: Error occurred updating booking";
+        }
+    }
+
+    private String handleAddObservingClient(DatagramPacket request) {
+        try {
+            String requestId = "unmarshallString(request.getData())";
+            if (requestId == "exists in cache") {
+                return "retrieve response from cache";
+            }
+            String facilityName = "unmarshallString(request.getData())";
+            InetAddress clientAddress = request.getAddress();
+            int clientPort = request.getPort();
+            int durationInMin = 30; // unmarshallInt(request.getData());
+            facilitiesBookingSystem.addObservingClient(facilityName, clientAddress, clientPort, durationInMin);
+            return "Successfully added to observing list";
+        } catch (FacilityNotFoundException e) {
+            return "404: Facility not found";
+        }
+    }
+
+    private String handleHeartbeat() {
+        return "Request received by server";
+    }
+
+    private String generateClientIdFromOrigin(DatagramPacket request) {
+        return request.getAddress().getHostAddress() + ":" + request.getPort();
     }
 
     /**
